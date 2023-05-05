7bdf14624e4503cb3acc6ebec99299cfee67e76f
karencfisher <karencfisher@hotmail.com>
2023-05-04 19:37:25 -0600

    refactor repo

--- src/schema.sql
+++ src/schema.sql
@@ -15,24 +15,23 @@ CREATE TABLE topics (
 );
 
 DROP TABLE IF EXISTS raw_reviews;
-CREATE TABLE "raw_reviews" (
-	"review_id"	INTEGER PRIMARY KEY,
-	"source_id", INTEGER,
-	"pub_date"	DATETIME,
-	"title"	TEXT,
-	"review_text"	TEXT,
-	"rating"	INTEGER,
-	FOREIGN KEY("source_id") REFERENCES sources(source_id)
+CREATE TABLE raw_reviews (
+	review_id	INTEGER PRIMARY KEY,
+	source_id, INTEGER,
+	pub_date	DATETIME,
+	title	TEXT,
+	review_text	TEXT,
+	rating	INTEGER,
+	FOREIGN KEY(source_id) REFERENCES sources(source_id)
 );
 
 DROP TABLE IF EXISTS cooked_reviews;
-CREATE TABLE "cooked_reviews" (
+CREATE TABLE cooked_reviews (
 	review_id INTEGER,
 	topic_id INTEGER,
 	angry INTEGER,
 	sentiment INTEGER,
 	summary TEXT,
-	FOREIGN KEY("review_id") REFERENCES raw_reviews(review_id),
-	FOREIGN KEY("topic_id") REFERENCES topics(topic_id)
-	
+	FOREIGN KEY(review_id) REFERENCES raw_reviews(review_id),
+	FOREIGN KEY(topic_id) REFERENCES topics(topic_id)
 );
